170284e0b42f5246ba34b73a6e7100782c03d16c
Adolfo <psyal3@nottingham.ac.uk>
2020-08-06 12:07:16 +0200

    added presentation cards for featured projects

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,34 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
     */
+
+   $(function() {
+
+    $(".progress").each(function() {
+  
+      var value = $(this).attr('data-value');
+      var left = $(this).find('.progress-left .progress-bar');
+      var right = $(this).find('.progress-right .progress-bar');
+  
+      if (value > 0) {
+        if (value <= 50) {
+          right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+        } else {
+          right.css('transform', 'rotate(180deg)')
+          left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+        }
+      }
+  
+    })
+  
+    function percentageToDegrees(percentage) {
+  
+      return percentage / 100 * 360
+  
+    }
+  
+  });
+  
    $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
     var next = $(this).next();
     if (!next.length) {
